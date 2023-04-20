ec5767c23e7391d568cb709167fbbc50c56a055c
fp5774s <fachrialdimaspp15@gmail.com>
2023-04-20 15:03:49 +0200

    BFS & DFS

--- 1_PathPlanning/main.py
+++ 1_PathPlanning/main.py
@@ -2,7 +2,12 @@ from graph import *
 from utils import *
 from queue import *
 
-def BF_search(graph, start, goal):
+
+#
+#Breadth First Search algorithm
+#return a list of the path, that should be taken based of the algorithm
+#
+def BF_Search(graph, start, goal):
     queue = Queue()
     queue.put(graph.nodes[next((i for i,v in enumerate(graph.nodes) if v.name == start), -1)])
     explored = []
@@ -14,24 +19,76 @@ def BF_search(graph, start, goal):
 
     while not queue.empty():
         node = queue.get()
-        path.append(node.name)
+        neighbours = node.edges  
 
-        if node.name not in explored:
-            neighbours = node.edges  
-
-            for neighbour in neighbours :
-                new_path = list(path)
-                new_path.append(neighbour.end)
-                queue.put(graph.nodes[next((i for i,v in enumerate(graph.nodes) if v.name == new_path),-1)])
-
-                if neighbour == goal:
-                    return new_path
-            explored.append(node.name)
+        for neighbour in neighbours :
+            if neighbour.end.name not in explored:
+                queue.put(neighbour.end)
+                neighbour.end.parent=node
+                if neighbour.end.name == goal:
+                        new_path = list(path)
+                        new_path.append(neighbour.end.name)
+                        parent = node
+                        while parent !=0:
+                             new_path.append(parent.name)
+                             parent=parent.parent
+                        new_path.reverse()
+                        return new_path
+        explored.append(node.name)
     
-    print(explored)
+    print("explored: ",explored)
     print("connecting path doesn't exist")
     return path
 
+#
+#Depth First Search algorith
+#return a list of the path, that should be taken based of the algorithm
+#
+def DF_Search(graph, start, goal):
+    queue = LifoQueue()
+    queue.put(graph.nodes[next((i for i,v in enumerate(graph.nodes) if v.name == start), -1)])
+    explored = []
+    path=[]
+
+    if start == goal:
+        print("Start and goal is the same")
+        return path
+
+    while not queue.empty():
+        node = queue.get()
+        neighbours = node.edges  
+
+        for neighbour in neighbours :
+            if neighbour.end.name not in explored:
+                queue.put(neighbour.end)
+                neighbour.end.parent=node
+                if neighbour.end.name == goal:
+                        new_path = list(path)
+                        new_path.append(neighbour.end.name)
+                        parent = node
+                        while parent !=0:
+                             new_path.append(parent.name)
+                             parent=parent.parent
+                        new_path.reverse()
+                        return new_path
+        explored.append(node.name)
+
+#
+#cost function
+#return the cost of the chosen path
+#
+
+def cost(graph,path):
+    node = graph.nodes[next((i for i,v in enumerate(graph.nodes) if v.name == path[0]), -1)]
+    cost = 0
+    for city in path :
+        if node.name == city:
+          continue
+        for edge in node.edges:
+            if edge.end.name == city:
+                cost += edge.value
+                node=edge.end
+    return cost
 
 
 romania = Graph( ['Or', 'Ne', 'Ze', 'Ia', 'Ar', 'Si', 'Fa', 
@@ -52,5 +109,7 @@ romania = Graph( ['Or', 'Ne', 'Ze', 'Ia', 'Ar', 'Si', 'Fa',
 			('Hi', 'Ef', 86)
 		     ] )
 
-path=BF_search(romania, 'Bu' , 'Ti')
+#romania.print()
+path=DF_Search(romania, 'Bu' , 'Ti')
 print(path)
+print("cost: ",cost(romania,path))
